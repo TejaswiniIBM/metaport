5c1d4c3e0cb4730e8989e93647f2215901c739e9
TejaswiniIBM <tejaswini.r1@ibm.com>
2026-05-04 22:59:23 +0530

    Uncomment Build stage in cicd.groovy
    Signed-off-by: TejaswiniIBM <tejaswini.r1@ibm.com>

--- cicd.groovy
+++ cicd.groovy
@@ -9,9 +9,9 @@ node('linux')
                         extensions: [],
                         userRemoteConfigs: [[url: "https://github.com/TejaswiniIBM/metaport.git"]]])
         }
-  // stage('Build') {
-  //   build job: 'Port-Build-Test', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/TejaswiniIBM/metaport.git'), string(name: 'PORT_DESCRIPTION', value: 'A set of utilities for z/OS Open Tools' )]
-  // }
+  stage('Build') {
+    build job: 'Port-Build-Test', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/TejaswiniIBM/metaport.git'), string(name: 'PORT_DESCRIPTION', value: 'A set of utilities for z/OS Open Tools' )]
+  }
    stage('Publish Test') {
     // Add your publish script testing here
     // Example:
