1d0c46d4a760afce4e6714dc0500c516df198375
TejaswiniIBM <tejaswini.r1@ibm.com>
2026-05-04 23:12:35 +0530

    Clean up publish stage in cicd.groovy
    Removed commented-out publish script testing instructions.
    
    Signed-off-by: TejaswiniIBM <tejaswini.r1@ibm.com>

--- cicd.groovy
+++ cicd.groovy
@@ -13,11 +13,6 @@ node('linux')
     build job: 'Port-Build-Test', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/TejaswiniIBM/metaport.git'), string(name: 'PORT_DESCRIPTION', value: 'A set of utilities for z/OS Open Tools' )]
   }
    stage('Publish Test') {
-    // Add your publish script testing here
-    // Example:
-    // sh './scripts/test-publish.sh'
-    // or
-    // build job: 'Port-Publish-Test', parameters: [...]
       build job: 'Port-Publish-Test', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/TejaswiniIBM/metaport.git'), string(name: 'PORT_DESCRIPTION', value: 'A set of utilities for z/OS Open Tools' )]
   }
 }
